262edbad64b275c8ad567e60ec5380a29933ee39
Julius <juliusarendt@icloud.com>
2023-05-08 15:34:19 +0200

    refactored PlayerTest.java

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,7 +1,6 @@
 import gameobjects.Player;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,22 +9,16 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
-    private JSONObject jsonObject;
     private Player player;
 
     @BeforeEach
     void setup() throws JSONException {
-        this.jsonObject = new JSONObject("{\"name\":\"Aremju\",\"webSocketID\":\"blablabla\"}");
         this.player = new Player("Aremju","blablabla",0,new ArrayList<>());
     }
 
-    @AfterEach
-    void tearDown() {
-        this.jsonObject = null;
-    }
-
     @Test
-    void creationSuccessful() {
+    void creationSuccessful() throws JSONException {
+        JSONObject jsonObject = new JSONObject("{\"name\":\"Aremju\",\"webSocketID\":\"blablabla\"}");
         Player anotherPlayer = new Player(jsonObject);
         assertEquals(anotherPlayer, player);
     }
