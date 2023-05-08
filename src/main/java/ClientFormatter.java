113063aa07207d26da2b5ecf4b4376d2e3128b7d
Julius <juliusarendt@icloud.com>
2023-05-08 19:37:42 +0200

    Added Feature: different colors for different levels

--- src/main/java/ClientFormatter.java
+++ src/main/java/ClientFormatter.java
@@ -1,9 +1,21 @@
+import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.SimpleFormatter;
 
 public class ClientFormatter extends SimpleFormatter {
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_GREEN = "\u001B[32m";
     @Override
     public String format(LogRecord record) {
-        return record.getLoggerName() + " -> " + record.getMessage() + "\n\n";
+        StringBuilder builder = new StringBuilder();
+        if (record.getLevel() == Level.INFO) {
+            builder.append(ANSI_GREEN);
+        }
+        builder.append(record.getLoggerName());
+        builder.append(" -> ");
+        builder.append(record.getMessage());
+        builder.append(ANSI_RESET);
+        builder.append("\n\n");
+        return builder.toString();
     }
 }
