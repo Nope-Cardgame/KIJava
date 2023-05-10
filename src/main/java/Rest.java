08c215c5d9c220a6393c0c4e2bd44ba7971a3542
MarianK99 <Mariankogeikg@gmx.de>
2023-05-10 20:49:20 +0200

    added parameter to request-method

--- src/main/java/Rest.java
+++ src/main/java/Rest.java
@@ -41,4 +41,34 @@ public class Rest {
             LOG.info("Response Body: \n" + response);
         }
     }
+
+    public void request(String urlString, String token, RequestType requestType, int id) throws IOException {
+
+        LOG.info("current Connection: \n" + urlString);
+
+        URL obj = new URL(urlString + id + "/");
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        // Invoke HTTP connection with POST-Request and application/json for sending
+        // element and web token
+        con.setRequestMethod(requestType.toString());
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+        con.setRequestProperty("Authorization", "Bearer " + token);
+
+        con.setDoOutput(true);
+
+        int responseCode = con.getResponseCode();
+
+        LOG.info("Response Code: \n" + responseCode);
+        StringBuilder response = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+            String inputLine;
+            // build response and print it
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            LOG.info("Response Body: \n" + response);
+        }
+    }
 }
