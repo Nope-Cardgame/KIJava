4e0f77a1f03facaddb0c84ee16ef5a9c4b4ddd8e
Julius <juliusarendt@icloud.com>
2023-05-10 18:03:10 +0200

    DisqualifyPlayer finished (+ Test)

--- src/main/java/gameobjects/actions/DisqualifyPlayer.java
+++ src/main/java/gameobjects/actions/DisqualifyPlayer.java
@@ -1,10 +1,12 @@
 package gameobjects.actions;
 
+import com.google.gson.Gson;
 import gameobjects.Player;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DisqualifyPlayer extends Action{
+
     private Player player;
     public DisqualifyPlayer(String type, String explanation, Player player) {
         super(type, explanation);
@@ -13,10 +15,25 @@ public class DisqualifyPlayer extends Action{
 
     public DisqualifyPlayer(String jsonString) {
         super(jsonString);
+        try {
+            JSONObject jsonObject = new JSONObject(jsonString);
+            String playerJson = jsonObject.getJSONObject("player").toString();
+            this.player = new Player(playerJson);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String toJSON() {
-        return null;
+        return new Gson().toJson(this);
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
     }
 }
