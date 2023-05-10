e3096b3881697681901ff918ce36b6ab6d7fc1a9
Alexander Lauruhn <alexander.timm@fh-bielefeld.de>
2023-05-10 23:02:13 +0200

    Gui implemented for Log In

---
+++ src/main/java/gameobjects/view/Console.java
@@ -0,0 +1,13 @@
+package gameobjects.view;
+import gameobjects.cards.Card;
+
+import java.util.List;
+
+public class Console implements  View{
+
+    public void showCards(List<Card> cards){
+        for (Card card: cards){
+        }
+
+    }
+}
