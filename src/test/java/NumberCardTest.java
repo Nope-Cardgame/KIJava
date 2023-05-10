e3edc040ac0d993c6ae52c209cf9e87abdef13b0
Julius <juliusarendt@icloud.com>
2023-05-10 16:29:33 +0200

    Split CardTest.java in two different Test sources

--- src/test/java/NumberCardTest.java
+++ src/test/java/NumberCardTest.java
@@ -1,4 +1,3 @@
-import gameobjects.cards.ActionCard;
 import gameobjects.cards.Card;
 import gameobjects.cards.NumberCard;
 import org.junit.jupiter.api.Test;
@@ -8,31 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CardTest {
-    @Test
-    void actionCardCreation() {
-        List<String> color = new ArrayList<>();
-        color.add("red");
-        Card actionCard = new ActionCard("nominate", color, "red nominate");
-        // Necessary step to check whether polymorphism works or not
-        ActionCard staticActionCard = (ActionCard) actionCard;
-        ActionCard anotherActionCard = new ActionCard("{\"type\":\"nominate\",\"color\":[\"red\"],\"name\":\"red nominate\"}");
-        // Check if values are equal
-        assertEquals(staticActionCard.getCardType(),anotherActionCard.getCardType());
-        assertEquals(staticActionCard.getName(),anotherActionCard.getName());
-        assertEquals(staticActionCard.getColors(),anotherActionCard.getColors());
-    }
-
-    @Test
-    void actionCardToJSON() {
-        List<String> color = new ArrayList<>();
-        color.add("red");
-        Card actionCard = new ActionCard("nominate", color, "red nominate");
-        String expectedJSON = "{\"color\":[\"red\"],\"type\":\"nominate\",\"name\":\"red nominate\"}";
-        // sequence of json dictionaries doesn't matter
-        assertEquals(expectedJSON,actionCard.toJSON());
-    }
-
+public class NumberCardTest {
     @Test
     void numberCardCreation() {
         List<String> color = new ArrayList<>();
