a533e57b246a3080374879c5bf1cd80135bbb96d
Julius <juliusarendt@icloud.com>
2023-05-10 18:03:55 +0200

    Added Methods for explanation

--- src/main/java/gameobjects/actions/Action.java
+++ src/main/java/gameobjects/actions/Action.java
@@ -2,15 +2,29 @@ package gameobjects.actions;
 
 import com.google.gson.Gson;
 import gameobjects.Jsonable;
+
 public abstract class Action implements Jsonable {
     private String type;
     private String explanation;
 
+    /**
+     * Standard-Constructor for an Action-Instance
+     * @param type the type of this Action
+     * @param explanation
+     */
     public Action(String type, String explanation) {
         this.type = type;
         this.explanation = explanation;
     }
 
+    /**
+     * Constructs an Action-Instance
+     * with a json-String
+     *
+     * @param jsonString a JSON-formatted String which contains
+     *                   - a "type"-property
+     *                   - an "explanation"-property
+     */
     public Action(String jsonString) {
         Gson gson = new Gson();
         this.explanation = gson.fromJson(jsonString, getClass()).getExplanation();
