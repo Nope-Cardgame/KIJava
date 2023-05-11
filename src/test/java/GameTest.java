3e9c88a1de103b45946ba81f2410828507847a1b
Julius <juliusarendt@icloud.com>
2023-05-11 09:01:34 +0200

    Added GameTest and TournamentTest

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,14 @@
+import gameobjects.Game;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void gameCreation() {
+        Game game = new Game("{\"id\":\"abc123\"}");
+    }
+
+    @Test
+    void gameJson() {
+
+    }
+}
