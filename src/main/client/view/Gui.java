319e251d7fd8f957cb519bbcf8e45b82945a50ac
Alexander Lauruhn <alexander.timm@fh-bielefeld.de>
2023-05-22 12:19:19 +0200

    change amount of example cards

--- src/main/client/view/Gui.java
+++ src/main/client/view/Gui.java
@@ -15,16 +15,16 @@ public final class Gui extends JFrame {
     private static Gui INSTANCE;
 
     private NumberCard frontCard = new NumberCard("number", 1, Arrays.asList("red", "blue"), "card1");
-    private List<NumberCard> ownCards = new ArrayList<NumberCard>();
-    private JTextField usernameTextfield = new JTextField();
-    private JLabel usernameLabel = new JLabel("User name:");
-    private JLabel passwortLabel = new JLabel("Password:");
-    private JPasswordField passwordfield = new JPasswordField();
-    private JButton loginButton = new JButton("Log in");
-    private JButton savaLoginData = new JButton("Save Data");
-    private ActionHandler act = new ActionHandler();
-    private JTable table = new JTable();
-    private ShowCards showCards = new ShowCards();
+    private List<NumberCard> ownCards = new ArrayList<NumberCard>(); // the cards on the hand
+    private JTextField usernameTextfield = new JTextField(); // the user can type in his name
+    private JLabel usernameLabel = new JLabel("User name:"); // label to descibe
+    private JLabel passwortLabel = new JLabel("Password:");// label to descibe
+    private JPasswordField passwordfield = new JPasswordField(); //passwordfield to not show pw
+    private JButton loginButton = new JButton("Log in"); // button for login
+    private JButton savaLoginData = new JButton("Save Data"); //button to save login data in txt document
+    private ActionHandler act = new ActionHandler(); // for the buttons
+    private JTable table = new JTable(); //shows all actions of game in a list
+    private ShowCards showCards = new ShowCards();// jpanel showing the cards with picutes
     DefaultTableModel model = new DefaultTableModel(new Object[][]{}, new String[]{"Nr.", "Player", "Action"});
     JScrollPane scroll= new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     public static Gui getInstance() {
@@ -37,16 +37,11 @@ public final class Gui extends JFrame {
     public Gui () {
         getOwnCards().add(new NumberCard("number", 1, Arrays.asList("red", "blue"), "card1"));
         getOwnCards().add(new NumberCard("number", 3, List.of("yellow"), "card2"));
-        getOwnCards().add(new NumberCard("number", 2, List.of("blue"), "card3"));
-        getOwnCards().add(new NumberCard("number", 1,  Arrays.asList("turquoise", "red"), "card4"));
-        getOwnCards().add(new NumberCard("number", 3, Arrays.asList("yellow", "turquoise"), "card5"));
-        getOwnCards().add(new NumberCard("number", 3, List.of("yellow"), "card2"));
-
 
         showCards.setBounds(20,240,550,300); //showing the cards
         showCards.setVisible(true);
         add(showCards);
-        scroll.setVisible(false);
+        scroll.setVisible(false); //scroll bar for game table
         table.setModel(model);
         table.getColumnModel().getColumn(0).setPreferredWidth(5);
         table.setEnabled(true);
@@ -60,18 +55,18 @@ public final class Gui extends JFrame {
         loginButton.setBounds(250,30,120,30);
         loginButton.setVisible(true); // to log in
         loginButton.addActionListener(act);
-        add(loginButton);
+        add(loginButton); // button for login
         savaLoginData.setBounds(250,70,120,30);
         savaLoginData.setVisible(true); // to save the user name and passwort
         savaLoginData.addActionListener(act);
-        add(savaLoginData);
+        add(savaLoginData); // button to save date in txt
         usernameTextfield.setBounds(120,30,120,30);
         usernameTextfield.setVisible(true);
         add(usernameTextfield);
 
         passwordfield.setBounds(120,70,120,30);
         passwordfield.setVisible(true);
-        add(passwordfield);
+        add(passwordfield); // pw is not visible for user
         passwortLabel.setBounds(10,70,120,30);
         passwortLabel.setVisible(true);
         add(passwortLabel);
@@ -79,7 +74,7 @@ public final class Gui extends JFrame {
         usernameLabel.setVisible(true);
         add(usernameLabel);
         add(new JLabel());
-        setSize(700,700);
+        setSize(700,700); //square size
         setVisible(true);
 
 
@@ -88,6 +83,11 @@ public final class Gui extends JFrame {
     public List<NumberCard> getOwnCards() {
         return ownCards;
     }
+
+    /**
+     * update gui  with current cards
+     * @param ownCards
+     */
     public void refresh(List<NumberCard> ownCards) {
         ownCards = ownCards;
         showCards.repaint();
