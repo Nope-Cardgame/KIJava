f88c8a9abd7e88aa27a79eed274bbebfbdba6238
MarianK99 <Mariankogeikg@gmx.de>
2023-05-29 22:54:27 +0200

    changed with max amount of player invitation per game to 6

--- src/main/client/command/ActionHandler.java
+++ src/main/client/command/ActionHandler.java
@@ -68,7 +68,7 @@ public class ActionHandler implements ActionListener {
                 socketIDs[i] = (String) Gui.getInstance().getAddedPlayerToInviteTable().getValueAt(i, 1);
              }
              try {
-                 if(playernames.length > 0) rest.invitePlayer(playernames, socketIDs);
+                 if(playernames.length > 0 && playernames.length < 7) rest.invitePlayer(playernames, socketIDs);
              }catch (IOException | JSONException ex) {
                  throw new RuntimeException(ex);
             }
